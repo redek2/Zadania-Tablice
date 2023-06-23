f24c24379df4bd0d97d4b1c1a3f75484c716aef4
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-24 01:18:46 +0200

    Add files via upload

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class MyProgram
+{
+    private static Random random = new Random();
+
+    public static void Main(string[] args)
+    {
+        int rOZMIAR, i;
+
+        rOZMIAR = 10;
+        int[] tab = new int[rOZMIAR];
+
+        for (i = 0; i <= rOZMIAR - 1; i++)
+        {
+            tab[i] = random.Next(100);
+        }
+        for (i = 0; i <= rOZMIAR - 1; i++)
+        {
+            Console.WriteLine(tab[i]);
+        }
+    }
+}
